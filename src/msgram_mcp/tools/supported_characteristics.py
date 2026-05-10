88adbf8737ee3088f19731fd0ac01fdb86330626
João Antonio Ginuino Carvalho <joaoseisei@gmail.com>
2026-05-10 20:15:05 -0300

    feat: adiciona caracteristicas

--- src/msgram_mcp/tools/supported_characteristics.py
+++ src/msgram_mcp/tools/supported_characteristics.py
@@ -4,12 +4,23 @@ from mcp.server.fastmcp import FastMCP
 
 def register_tools(mcp: FastMCP, service: str):
 
-    @mcp.tool()
-    def listar_subcaracteristicas() -> list[dict]:
-        """Lista todas as subcaracterísticas suportadas pelo MeasureSoftGram."""
+    def query(url: str) -> list[dict]:
         response = httpx.get(
-            f"{service}supported-subcharacteristics/",
-            headers={"accept": "application/json"},
+            f"{url}",
+            headers={
+                "accept": "application/json"
+            },
         )
         response.raise_for_status()
         return response.json()["results"]
+
+    @mcp.tool()
+    def listar_caracteristicas() -> list[dict]:
+        """Lista todas as características suportadas pelo MeasureSoftGram."""
+        return query(f"{service}supported-characteristics/")
+
+
+    @mcp.tool()
+    def listar_subcaracteristicas() -> list[dict]:
+        """Lista todas as subcaracterísticas suportadas pelo MeasureSoftGram."""
+        return query(f"{service}supported-subcharacteristics/")
