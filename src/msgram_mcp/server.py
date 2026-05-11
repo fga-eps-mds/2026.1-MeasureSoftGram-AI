a939e7494f9794f04f58071eeae269e0d6bf4820
João Antonio Ginuino Carvalho <joaoseisei@gmail.com>
2026-05-10 21:38:29 -0300

    feat: adiciona get de releases

--- src/msgram_mcp/server.py
+++ src/msgram_mcp/server.py
@@ -5,6 +5,7 @@ from mcp.server.fastmcp import FastMCP
 from msgram_mcp.tools.supported_characteristics import register_tools as supported_characteristics
 from msgram_mcp.tools.organizations import register_tools as organization_register
 from msgram_mcp.tools.supported_metrics import register_tools as register_metrics_tools
+from msgram_mcp.tools.releases import register_tools as releases_tools
 from msgram_mcp.auth.msgram_auth import msgram_auth
 
 
@@ -34,6 +35,7 @@ def create_server(settings: Settings) -> FastMCP:
     supported_characteristics(mcp_server, service=settings.service)
     organization_register(mcp_server, service=settings.service, token=settings.token)
     register_metrics_tools(mcp_server, service=settings.service)
+    releases_tools(mcp_server, service=settings.service, token=settings.token)
 
     return mcp_server
 
